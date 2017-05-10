0f6b524f33ea023956e408230d1ef0c052d1b7f5
Farzan Erlik Nowruzi <farzan87@gmail.com>
2017-05-10 17:14:36 -0400

    updates to prepare dataset

--- Data_IO/dataset_prepare.py
+++ Data_IO/dataset_prepare.py
@@ -34,79 +34,113 @@ def image_process_subMean_divStd_n1p1(img):
     out = (2*((out-out.min())/(out.max()-out.min())))-1
     return out
 
-def perturb_writer( ID, idx,
-                    imgOrig, imgPatchOrig, imgPatchPert, HAB, pOrig,
+def perturb_writer( ID,
+                    pclA, pclB,
+                    imgDepthA, imgDepthB,
+                    tMatTarget,
                     tfRecFolder):
+    '''
+    ID: python list with size 2
+    pclA, pclB: numpy matrix of size nx4, mx4
+    imgDepthA, imgDepthB: numpy matrix of size 128x512
+    tmatTarget: numpy matrix of size 4x4
+    tfRecFolder: folder name
+    '''
     filename = str(ID) + "_" + str(idx)
     fileID = [ID, idx]
     tfrecord_io.tfrecord_writer(imgOrig, imgPatchOrig, imgPatchPert, pOrig, HAB, tfRecFolder, filename, fileID)
     return
 
 ################################
-def transform_pcl(xyzi_col, tMat):
-    i_col = xyzi_col[3]
+def transform_pcl_2_origin(xyzi_col, tMat2o):
+    '''
+    pointcloud i, and tMat2o i to origin
+    '''
+    intensity_col = xyzi_col[3]
     xyz1 = xyzi_col.copy()
     xyz1[3] *= 0
     xyz1[3] += 1
-    xyz1 = np.matmul(tMat, xyz1)
-    xyz1[3] = i_col
+    xyz1 = np.matmul(tMat2o, xyz1)
+    xyz1[3] = intensity_col
     return xyz1
+################################
+def _get_tMat_A_2_B(tMatA2o, tMatB2o):
+    '''
+    tMatA2o A -> O (source pcl is in A), tMatB2o B -> O (target pcl will be in B)
+    return tMat A -> B
+    '''
+    # tMatA2o: A -> Orig
+    # tMatB2o: B -> Orig ==> inv(tMatB2o): Orig -> B
+    # inv(tMatB2o) * tMatA2o : A -> B
+    return np.matmul(np.matmul(np.linalg.inv(tMatB2o), tMatA2o)
 ################################
 def _get_correct_tmat(poseRow):
     return (np.array(np.append(poseRow, [0, 0, 0, 1]), dtype=np.float32)).reshape([4,4])
 ################################
 def _get_pcl(filePath):
+    '''
+    Get a bin file address and read it into a numpy matrix
+    Converting LiDAR coordinate system to Camera coordinate system for pose transform
+    '''
     f = open(filePath, 'rb')
     i = 0
+    j = 0
     pclpoints = list()
     # Camera: x = right, y = down, z = forward
     # Velodyne: x = forward, y = left, z = up
     # GPS/IMU: x = forward, y = left, z = up
     # Velodyne -> Camera (transformation matrix is in camera order)
-    #print('Reading X = -y, Y = -z, Z = x, i = 3')
+    #print('Reading X = -y,         Y = -z,      Z = x,     i = 3')
+    #               0 = left/right, 1 = up/down, 2 = in/out
     while f.readable():
         xyzi = f.read(4*4)
         if len(xyzi) == 16:
-            row = struct.unpack('f'*4, f.read(4*4))
-            pclpoints.append([-1*row[1], -1*row[2], row[0], row[3]])
-            i += 1
+            row = struct.unpack('f'*4, xyzi)
+            if j%1 == 0:
+                pclpoints.append([-1*row[1], -1*row[2], row[0], row[3]])
+                i += 1
         else:
+            print('num pclpoints =', i)
             break
+        j += 1
         #if i == 15000:
         #    break
     f.close()
     # convert to numpy
     xyzi = np.array(pclpoints, dtype=np.float32)
     return xyzi.transpose()
+
 ################################
-def process_dataset(startTime, durationSum, pclFilenames, pclFilenames, tfRecFolder, poseFile, i):
+def process_dataset(startTime, durationSum, pclFilenames, poseFile, tfRecFolder, i):
     '''
-    point cloud is moved to i'th frame
-    tmat_0r, tmat_1r => tmat10 = tmat_1r*inv(tmat_0r)
+    pclFilenames: list of pcl file addresses
+    poseFile: includes a list of pose files to read
+    point cloud is moved to i+1'th frame:
+        tMatAo (i): A->0
+        tMatBo (i+1): B->0
+        tMatAB (target): A->B  (i -> i+1) 
     '''
     # get i
-    xyzi_0 = _get_pcl(pclFilenames[i])
-    if i==0:
-        pose_0 = np.identity(4)
-    else:
-        pose_0 = _get_correct_tmat(poseFile[i])
-    imgDepth_0 = _get_depthMap(xyzi_0)
+    xyzi_A = _get_pcl(pclFilenames[i])
+    pose_Ao = _get_correct_tmat(poseFile[i])
+    imgDepth_A = _get_depthMap(xyzi_A)
     # get i+1
-    xyzi_1 = _get_pcl(pclFilenames[i+1])
-    pose_1 = _get_correct_tmat(poseFile[i+1])
-    imgDepth_1 = _get_depthMap(xyzi_1)
-    pose_10 = np.matmul(pose_1, np.linalg.inv(pose_0))
+    xyzi_B = _get_pcl(pclFilenames[i+1])
+    pose_Bo = _get_correct_tmat(poseFile[i+1])
+    imgDepth_B = _get_depthMap(xyzi_B)
+    # get target pose
+    pose_AB = _get_tMat_A_2_B(pose_Ao, pose_Bo)
+    #
     fileID = [i-1, i]
     perturb_writer(fileID,
-                   xyzi_0, xyzi_1,
-                   imgDepth_0, imgDepth_1,
-                   pose_10,
+                   xyzi_A, xyzi_B,
+                   imgDepth_A, imgDepth_B,
+                   pose_AB,
                    tfRecFolder)
-
 
 ################################
 def _get_pose_data(posePath):
-    return np.loadtxt(open(posePath, "r"), delimiter=" ", skiprows=1)
+    return np.loadtxt(open(posePath, "r"), delimiter=" ")
 ################################
 def _get_pcl_folder(pclFolder, seqID):
     return pclFolder + seqID + '/' + 'velodyne/'
@@ -126,7 +160,7 @@ def prepare_dataset(datasetType, readPath, seqIDs, tfRecFolder):
         pclFilenames.sort()
         startTime = time.time()
         num_cores = multiprocessing.cpu_count()
-        Parallel(n_jobs=num_cores)(delayed(process_dataset)(startTime, durationSum, pclFilenames, pclFilenames, tfRecFolder, poseFile, i) for i in range(len(pclFilenames-1)))
+        Parallel(n_jobs=num_cores)(delayed(process_dataset)(startTime, durationSum, pclFilenames, poseFile, tfRecFolder, i) for i in range(len(pclFilenames-1)))
     print('Done')
 
 ############# PATHS
